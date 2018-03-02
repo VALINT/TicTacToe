3a4ae6af44d96bfa49366a08178e398a1fe13722
VALINT <33782876+VALINT@users.noreply.github.com>
2018-03-02 21:16:46 +0200

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,22 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 02.11.2016 16:51:05
+ *  Author: VAL
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+void StartScreen();
+void BuildScreen();
+void ButtonProcessor();
+void printer();
+void Menu();
+void LCD_Start();
+void LCD_Menu();
+void LCD_Name();
+
+
+#endif /* INCFILE1_H_ */
